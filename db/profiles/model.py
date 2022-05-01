c31d90e359cb99b164edbf82cbccc2c9f337bc21
Alex Kor <vlc361358@gmail.com>
2022-05-01 17:37:30 +0200

    db: init profiles

---
+++ db/profiles/model.py
@@ -0,0 +1,12 @@
+from peewee import CharField, ForeignKeyField, BooleanField
+from db.base.base_model import BaseModel
+
+
+class Profile(BaseModel):
+    is_bot: bool = BooleanField(default=False)
+    first_name: str = CharField(null=False)
+    username: str = CharField(null=False)
+    lang_code: str = CharField(null=False)
+
+    class Meta:
+        table_name = 'profiles'
