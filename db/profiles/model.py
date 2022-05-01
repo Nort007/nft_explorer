a4a0f41f1f71085dc6c9a0ffbcda792530079fa5
Alex Kor <vlc361358@gmail.com>
2022-05-01 20:13:54 +0200

    hotfix db

--- db/profiles/model.py
+++ db/profiles/model.py
@@ -1,8 +1,9 @@
-from peewee import CharField, BooleanField
+from peewee import CharField, BooleanField, IntegerField
 from db.base.base_model import BaseModel
 
 
 class ProfileModel(BaseModel):
+    user_id: int = IntegerField(unique=True)
     is_bot: bool = BooleanField(default=False)
     first_name: str = CharField(null=False)
     username: str = CharField(null=False)
