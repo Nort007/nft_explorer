161d6dd2e01e800cdf40633ff8823c67a559fb70
Alex Kor <vlc361358@gmail.com>
2022-05-22 18:49:52 +0200

    part of telegram bot

--- bot_v01/handlers/users/purchases_new_nft.py
+++ bot_v01/handlers/users/purchases_new_nft.py
@@ -3,34 +3,38 @@ from aiogram.dispatcher import FSMContext
 from aiogram import types
 from bot_v01.keyboards.inline.menu import new_nft_menu
 from aiogram.dispatcher.filters.state import State, StatesGroup
+# from bot_v01.services.service import information_about_query_of_user
+from bot_v01.services.service_prepare_text import prepare_text_to_choose_option
+import json
 
 
 class NFTState(StatesGroup):
+    choose = State()
     name_or_address = State()
 
 
 async def new_nft_to_watchlist(q: Message):
-    """Обработчик команды /newnft.
-    Добавляет новый нфт в базу данных watchlist и далее привязывает автоматически к юзеру
-    в conditions с дефолтными значениями"""
+    """First handler of command if user sended /newnft"""
     await q.answer("Choose search format", reply_markup=new_nft_menu())
-    print()
-    # await NFTState.name_or_address.set()
 
 
-async def choosen_option(q: CallbackQuery):
-    """Обрабатывает нажатие и отсюда вызывается обновление пейлода по какому значению искать в апи информацию."""
+async def choosen_option(q: CallbackQuery, state: FSMContext):
+    """Takes the option from the pressing of user and put them in redis and also set the state(FSM)"""
     await q.answer(cache_time=10)
     data = q.data.split(':')
-    if data[-1] == 'name':
-        pass
-    elif data[-1] == 'address':
-        pass
-    print(data)
+    await state.update_data(choose=data[-1])
+    text = prepare_text_to_choose_option(data[-1])
+    await q.message.answer(text=text)
+    await NFTState.name_or_address.set()
 
 
-async def valid_data(q: types.Message, state: FSMContext):
+async def text_data_state(q: types.Message, state: FSMContext):
     """Проверяет валидность данных путем обработки через запрос по апи и возвращение результата"""
     # await call.answer(cache_time=10)
     await q.answer("Получены данные. Проверяй редис.")
-    curr_data = await state.update_data(name_or_address=q.text)
+    await state.update_data(name_or_address=q.text)
+    curr_data = await state.get_data()
+    print(curr_data)
+    # t = information_about_query_of_user(option=curr_data['choose'], value=curr_data['name_or_address'])
+    # print(json.dumps(t['data']['collections'][0], indent=4, default=str))
+    # await state.finish()
--- bot_v01/services/service_prepare_text.py
+++ bot_v01/services/service_prepare_text.py
@@ -2,3 +2,10 @@ def text_to_edit_selected_condition(nft_name, button, condition):
     return "NFT: {}\n" \
            "Short_condition: {}\n" \
            "Condition (your_price, current_price): {}".format(nft_name, button, condition)
+
+
+def prepare_text_to_choose_option(choose):
+    if choose == 'name':
+        return "Insert nft name"
+    else:
+        return "Insert contract address"
