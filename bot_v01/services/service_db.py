906e4e5864a7e5655977673e8944f3de54dab669
Alex Kor <vlc361358@gmail.com>
2022-06-08 10:10:27 +0200

    updated some services of database

--- bot_v01/services/service_db.py
+++ bot_v01/services/service_db.py
@@ -1,4 +1,3 @@
-# from playhouse.shortcuts import model_to_dict
 import peewee
 from peewee import fn
 
@@ -15,7 +14,8 @@ def disclosure_fully_infromation_of_users():
     """Custom request to prepare information about users and their settings from other tables."""
     query = (ConditionModel
              .select(ConditionModel.profile_id, ConditionModel.watchlist_id, ConditionModel.gt, ConditionModel.ge, ConditionModel.lt, ConditionModel.le,
-                     ConditionModel.eq, WatchlistModel.name, WatchlistModel.address, MailingModel.public_channel, MailingModel.chat_id, MailingModel.active,
+                     ConditionModel.eq, WatchlistModel.name, WatchlistModel.address, WatchlistModel.slug, MailingModel.public_channel, MailingModel.chat_id,
+                     MailingModel.active,
                      ProfileModel.first_name, ProfileModel.username)
              .join(WatchlistModel, on=(ConditionModel.watchlist_id == WatchlistModel.id))
              .join(MailingModel, on=(ConditionModel.profile_id == MailingModel.profile_id))
@@ -53,7 +53,7 @@ def add_or_update_mailing(public_channel: str = None, chat_id: int = None, activ
 
 
 def is_banned(ban: bool, user_id: int):
-    """Проверяет на бан пользователя."""
+    """Check if user is banned or not."""
     if ban:
         return True
     else:
@@ -61,7 +61,7 @@ def is_banned(ban: bool, user_id: int):
 
 
 def compare_user_id_from_db(user: dict, msg: str):
-    """Сравнивает есть ли юзер в базе данных или нет."""
+    """Check if user is in database or not."""
     query = ProfileModel.select().where(ProfileModel.user_id == user['id'])
     try:
         q = query.get()
@@ -76,21 +76,8 @@ def compare_user_id_from_db(user: dict, msg: str):
     return True
 
 
-def query_option(option: str):
-    """В частом случае функция возвращает опции из /newnft когда юзер добавляет коллекцию себе по адресу или имени.
-    Возвращает в основном для where в запросе к базе данных."""
-    if option == 'name':
-        return WatchlistModel.name
-    elif option == 'address':
-        return WatchlistModel.address
-    else:
-        logger.error(f"Option '{option}' is not supported")
-        raise ValueError(f"Option '{option}' is not supported")
-
-
-def get_watchlist(user_pk: int = None, user_id: int = None, option: str = None, value: str = None):
-    """Список нфт отслеживаемые юзером, если нет параметров, то все коллекции отслеживаемые юзером."""
-    watchlist: dict = {}
+def get_watchlist(user_pk: int = None, user_id: int = None):
+    """List of tracked nft by user, if no parameters then return empty value."""
     if user_id is not None:
         query = (WatchlistModel
                  .select(WatchlistModel, ProfileWatchlistModel, ProfileModel)
@@ -109,8 +96,7 @@ def get_watchlist(user_pk: int = None, user_id: int = None, option: str = None, 
 
 
 def select_chosen_collection_by_user(user_id: int = None, option: str = None, value: str = None):
-    """Возвращает все или конкретную коллекцию отслеживаемые юзером."""
-    # where_option = query_option(option)
+    """Returns nft by name or slug otherwise by address."""
     if option == 'name':
         where_join = (fn.LOWER(WatchlistModel.name) == value.lower()) | (fn.LOWER(WatchlistModel.slug) == value.lower())
     elif option == 'address':
@@ -138,8 +124,7 @@ def all_wl():
 
 
 def get_information_of_selected_nft(value: str, option: str = 'name'):
-    """Возвращает информацию об конкретной нфт"""
-    # where_option = query_option(option)
+    """Returns information about selected nft."""
     if option == 'name':
         where_option = (fn.LOWER(WatchlistModel.name) == value) | (fn.LOWER(WatchlistModel.slug) == value)
     elif option == 'address':
@@ -155,14 +140,14 @@ def get_information_of_selected_nft(value: str, option: str = 'name'):
 
 
 def add_new_nft_collection(name: str = None, address: str = None, slug: str = None):
-    """Добавляет новую коллекцию нфт"""
+    """Adds a new nft collection."""
     add_new_collection = WatchlistModel.create(name=name, address=address, slug=slug)
     logger.info(f"Collection {add_new_collection.name} has been added")
     return {'id': add_new_collection.id, 'name': add_new_collection.name, 'address': add_new_collection.address}
 
 
 def get_information_of_condition(user_id: int, nft_wl_id: int):
-    """Возвращает информацию по условиям конкретного нфт для конкретного юзера"""
+    """Returns information of user conditions for the selected nft."""
     q = (ConditionModel
          .select(ConditionModel)
          .join(WatchlistModel)
@@ -178,7 +163,7 @@ def create_new_condition(user_pk_id: int, wl_pk_id: int):
 
 
 def update_selected_condition(user_pk: int, nft_pk: int, condition: str, value: str | float):
-    """Обновляет выбранное условие по нфт"""
+    """Updating the nft condition for the selected user."""
     q = (ConditionModel
          .update({f"{condition}": value})
          .where(ConditionModel.profile_id == user_pk)
@@ -187,7 +172,7 @@ def update_selected_condition(user_pk: int, nft_pk: int, condition: str, value: 
 
 
 def add_new_user(user_id: int, is_bot: bool, first_name: str, username: str, lang_code: str):
-    """Добавляет нового юзера в базу данных"""
+    """Add a new user to database."""
     user, created = ProfileModel.get_or_create(user_id=user_id, is_bot=is_bot, first_name=first_name, username=username, lang_code=lang_code)
     logger.info(f"User has been added: {created}")
     return created
