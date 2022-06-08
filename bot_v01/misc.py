f08f4f959527011e6378335db1302a3c68f3c8de
Alex Kor <vlc361358@gmail.com>
2022-06-08 10:13:02 +0200

    added new fmt for a debug

--- bot_v01/misc.py
+++ bot_v01/misc.py
@@ -8,7 +8,7 @@ from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG')
+coloredlogs.install(level='DEBUG', fmt='%(asctime)s: %(levelname)s: [%(funcName)s: %(filename)s: %(module)s:] %(message)s')
 env = Path(os.path.dirname(__file__)).parent.resolve().joinpath('.env')
 if os.path.isfile(env):
     load_dotenv(env)
