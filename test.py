c1f3ba8e900809bc35f2d6cc26ee4f664ec33d98
Nort007 <vlc361358@gmail.com>
2022-06-17 10:43:00 +0200

    junk

--- test.py
+++
@@ -1,6 +0,0 @@
-from gem.gem_services import gem_collection
-
-test = gem_collection('0x5cc5b05a8a13e3fbdb0bb9fccd98d38e50f90c38')
-# print(json.dumps(['data'][0], indent=4, default=str))
-# print(test['data'][0]['name'], test['data'][0]['address'], test['data'][0]['slug'])
-print(test)
