cea27f9ec1b9000e76a1b67d2ca43912617de5c7
Nort007 <vlc361358@gmail.com>
2022-06-18 10:34:36 +0200

    load tg token from config

--- bot_v01/misc.py
+++ bot_v01/misc.py
@@ -1,19 +1,15 @@
 import logging
 import os
-from pathlib import Path
 
 import coloredlogs
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
-from dotenv import load_dotenv
+
+from core.config import TELEGRAM_TOKEN
 
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='DEBUG', fmt='%(asctime)s: %(levelname)s: [%(funcName)s: %(filename)s: %(module)s:] %(message)s')
-env = Path(os.path.dirname(__file__)).parent.resolve().joinpath('.env')
-if os.path.isfile(env):
-    load_dotenv(env)
-TG_TOKEN = os.getenv('TG_TOKEN')
-bot = Bot(token=TG_TOKEN)
+bot = Bot(token=TELEGRAM_TOKEN)
 storage = RedisStorage2(host=os.getenv('REDIS_HOST'), port=int(os.getenv('REDIS_PORT')), state_ttl=120, bucket_ttl=500, data_ttl=120)
 dp = Dispatcher(bot, storage=storage)
 
