a85993701923c22982ed0219f1658d0ab962738b
Nort007 <vlc361358@gmail.com>
2022-07-09 15:57:11 +0200

    tag change

--- api/routes.py
+++ api/routes.py
@@ -5,4 +5,4 @@ from api.endpoints.user import user
 
 route = APIRouter()
 route.include_router(login.router, tags=["authentication"], prefix="/v1")
-route.include_router(user.router, tags=["client"], prefix="/v1")
+route.include_router(user.router, tags=["user"], prefix="/v1")
