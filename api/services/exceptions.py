f15b29bc402618dfcb2fe3f3a2f1270982066ae9
Nort007 <vlc361358@gmail.com>
2022-07-10 00:41:04 +0200

    exceptions update

--- api/services/exceptions.py
+++ api/services/exceptions.py
@@ -1,20 +1,20 @@
 """Basic exceptions."""
 from fastapi import HTTPException
-from starlette.status import HTTP_403_FORBIDDEN
+from starlette.status import HTTP_403_FORBIDDEN, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 
 from core.logger_config import logger
 
 
 def one_time_code_exception(detail: str = 'Verify code is not correct') -> HTTPException:
     logger.debug(detail)
-    return HTTPException(status_code=400, detail=detail)
+    return HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=detail)
 
 
 def user_not_found(detail: str = 'User not found') -> HTTPException:
     logger.debug(detail)
-    return HTTPException(status_code=404, detail=detail)
+    return HTTPException(status_code=HTTP_404_NOT_FOUND, detail=detail)
 
 
-def user_forbidden(detail: str = 'Could not validate credentials'):
-    logger.debug(detail)
+def user_forbidden() -> HTTPException:
+    detail = {"message": "Could not validate credentials", "status": HTTP_403_FORBIDDEN}
     return HTTPException(status_code=HTTP_403_FORBIDDEN, detail=detail)
