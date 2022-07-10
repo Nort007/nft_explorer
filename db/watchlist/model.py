4f573498487d51e73e46092820caed9745ed5139
Nort007 <vlc361358@gmail.com>
2022-07-10 13:21:51 +0200

    added unique(btree)

--- db/watchlist/model.py
+++ db/watchlist/model.py
@@ -3,9 +3,9 @@ from db.base.base_model import BaseModel
 
 
 class WatchlistModel(BaseModel):
-    address: str = CharField(max_length=254, null=True)
+    address: str = CharField(max_length=254, null=True, unique=True)
     name: str = CharField(max_length=254, null=True)
-    slug: str = CharField(max_length=254, null=True)
+    slug: str = CharField(max_length=254, null=True, unique=True)
 
     class Meta:
         table_name = 'watchlist'
